8150f9199bdd8b46ef75a0691006349e4ed73738
Kshitij2011 <77619958+Kshitij2011@users.noreply.github.com>
2021-08-02 16:58:49 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
-
+noseX = 0;
+noseY = 0;
 function preload(){
-    
+    clown_nose = loadImage('https://i.postimg.cc/50szzndg/clown-faltoooooooooo.png');
 }
 
 function setup(){
-    canvas = creatCanvas(300, 300);
+    canvas = createCanvas(300, 300);
     canvas.center();
-    video = creatCapture(VIDEO);
+    video = createCapture(VIDEO);
     video.size(300, 300);
     video.hide();
 
@@ -23,14 +24,20 @@ function gotPoses(results)
     if (results.length > 0)
     {
         console.log(results);
-       console.log("nose x = " + results[0].pose.nose.x);
-       console.log("nose y = " + results[0].pose.nose.y); 
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+       console.log("nose x = " + noseX);
+       console.log("nose y = " + noseY); 
     }
 }
 
 function draw(){
     image(video, 0, 0, 300, 300);
+    fill(255,0,0);
+    stroke(255,0,0);
+    circle(noseX, noseY, 20);
     
+
 }
 
 function take_snapshot(){
